2c9225c36ca17cfe5e601939e55fbc518fea22a1
Aleks <san4es.kom@gmail.com>
2024-04-09 12:29:18 +0300

    feat: Initial commit

---
+++ src/components/Layout/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-violet font-Roboto text-h1">
+        Me u
+    </div>
+  );
+}
+
+export default App;
